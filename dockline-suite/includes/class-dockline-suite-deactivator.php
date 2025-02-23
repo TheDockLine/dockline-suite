d6643f43fb9e56fd1fdf20188cc5bcfdc9bc0a3f
Levi Mardis <levimardis@gmail.com>
2025-02-22 21:31:57 -0600

    First Commit

---
+++ dockline-suite/includes/class-dockline-suite-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://thedockline.com
+ * @since      1.0.0
+ *
+ * @package    Dockline_Suite
+ * @subpackage Dockline_Suite/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Dockline_Suite
+ * @subpackage Dockline_Suite/includes
+ * @author     Levi <levi@thedockline.com>
+ */
+class Dockline_Suite_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
