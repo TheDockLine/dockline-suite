1213f810b5992c33bb918c2346969a5e85d55367
Levi Mardis <levimardis@gmail.com>
2025-02-22 23:13:04 -0600

    Different updater

---
+++ includes/class-dockline-suite-github-updater.php
@@ -0,0 +1,163 @@
+<?php
+
+/**
+ * Handles GitHub integration for version control and updates.
+ *
+ * @link       https://thedockline.com
+ * @since      1.0.0
+ *
+ * @package    Dockline_Suite
+ * @subpackage Dockline_Suite/includes
+ */
+class Dockline_Suite_GitHub_Updater
+{
+    public $plugin_name;
+    public $version;
+    public $cache_key;
+    public $cache_allowed;
+
+    public function __construct($plugin_name, $version)
+    {
+        $this->version = $version;
+        $this->plugin_name = $plugin_name;
+        $this->cache_key = 'dockline_suite';
+        $this->cache_allowed = true;
+    }
+
+    public function request()
+    {
+
+        $remote = get_transient($this->cache_key);
+
+        if (false === $remote || !$this->cache_allowed) {
+
+            $remote = wp_remote_get(
+                'https://api.github.com/repos/TheDockLine/dockline-suite/releases/latest',
+                array(
+                    'timeout' => 10,
+                    'headers' => array(
+                        'Accept' => 'application/json',
+                    )
+                )
+            );
+
+            if (
+                is_wp_error($remote)
+                || 200 !== wp_remote_retrieve_response_code($remote)
+                || empty(wp_remote_retrieve_body($remote))
+            ) {
+                return false;
+            }
+
+            set_transient($this->cache_key, $remote, DAY_IN_SECONDS);
+        }
+
+        $remote = json_decode(wp_remote_retrieve_body($remote));
+
+        return $remote;
+    }
+
+
+    public function info($res, $action, $args)
+    {
+
+        // do nothing if you're not getting plugin information right now
+        if ('plugin_information' !== $action) {
+            return false;
+        }
+
+        // do nothing if it is not our plugin
+        if ($this->plugin_name !== $args->slug) {
+            return false;
+        }
+
+        // get updates
+        $remote = $this->request();
+
+        if (!$remote) {
+            return false;
+        }
+
+        $res = new stdClass();
+        $version = ltrim($remote['tag_name'], 'v');
+
+        //  $res->name = $remote->name;
+        //  $res->slug = $remote->slug;
+        //  $res->version = $remote->version;
+        //  $res->tested = $remote->tested;
+        //  $res->requires = $remote->requires;
+        //  $res->author = $remote->author;
+        //  $res->author_profile = $remote->author_profile;
+        //  $res->download_link = $remote->download_url;
+        //  $res->trunk = $remote->download_url;
+        //  $res->requires_php = $remote->requires_php;
+        //  $res->last_updated = $remote->last_updated;
+
+        $res->name = 'Dockline Suite';
+        $res->slug = 'dockline-suite';
+        $res->version = $version;
+        $res->tested = '5.8';
+        $res->requires = '3.0';
+        $res->author = $remote['author']['login'];
+        $res->author_profile = $remote['author']['html_url'];
+        $res->download_link = $remote['assets'][0]['browser_download_url'];
+        $res->trunk = $remote['assets'][0]['browser_download_url'];
+        $res->requires_php = '5.3';
+        $res->last_updated = $remote['published_at'];
+
+        $res->sections = array(
+            'description' => $remote['body'],
+            'changelog' => $remote['body']
+        );
+
+        if (!empty($remote->banners)) {
+            $res->banners = array(
+                'low' => $remote['banners']['low'],
+                'high' => $remote['banners']['high']
+            );
+        }
+
+        return $res;
+    }
+
+    public function update($transient)
+    {
+
+        if (empty($transient->checked)) {
+            return $transient;
+        }
+
+        $remote = $this->request();
+
+        if (
+            $remote
+            && version_compare($this->version, $remote->version, '<')
+            && version_compare($remote->requires, get_bloginfo('version'), '<')
+            && version_compare($remote->requires_php, PHP_VERSION, '<')
+        ) {
+            $res = new stdClass();
+            $res->slug = $this->plugin_name;
+            $res->plugin = dirname(plugin_basename(__DIR__)) . '/' . $this->plugin_name . '.php'; // misha-update-plugin/misha-update-plugin.php
+            $res->new_version = $remote->version;
+            $res->tested = $remote->tested;
+            $res->package = $remote->download_url;
+
+            $transient->response[$res->plugin] = $res;
+        }
+
+        return $transient;
+    }
+
+    public function purge()
+    {
+
+        if (
+            $this->cache_allowed
+            && 'update' === $options['action']
+            && 'plugin' === $options['type']
+        ) {
+            // just clean the cache when new plugin version is installed
+            delete_transient($this->cache_key);
+        }
+    }
+}
--- includes/class-dockline-suite.php
+++ includes/class-dockline-suite.php
@@ -27,7 +27,8 @@
  * @subpackage Dockline_Suite/includes
  * @author     Levi <levi@thedockline.com>
  */
-class Dockline_Suite {
+class Dockline_Suite
+{
 
 	/**
 	 * The loader that's responsible for maintaining and registering all hooks that power
@@ -66,8 +67,9 @@ class Dockline_Suite {
 	 *
 	 * @since    1.0.0
 	 */
-	public function __construct() {
-		if ( defined( 'DOCKLINE_SUITE_VERSION' ) ) {
+	public function __construct()
+	{
+		if (defined('DOCKLINE_SUITE_VERSION')) {
 			$this->version = DOCKLINE_SUITE_VERSION;
 		} else {
 			$this->version = '1.0.0';
@@ -78,7 +80,7 @@ class Dockline_Suite {
 		$this->set_locale();
 		$this->define_admin_hooks();
 		$this->define_public_hooks();
-
+		$this->define_update_hooks();
 	}
 
 	/**
@@ -97,33 +99,33 @@ class Dockline_Suite {
 	 * @since    1.0.0
 	 * @access   private
 	 */
-	private function load_dependencies() {
+	private function load_dependencies()
+	{
 
 		/**
 		 * The class responsible for orchestrating the actions and filters of the
 		 * core plugin.
 		 */
-		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'includes/class-dockline-suite-loader.php';
+		require_once plugin_dir_path(dirname(__FILE__)) . 'includes/class-dockline-suite-loader.php';
 
 		/**
 		 * The class responsible for defining internationalization functionality
 		 * of the plugin.
 		 */
-		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'includes/class-dockline-suite-i18n.php';
+		require_once plugin_dir_path(dirname(__FILE__)) . 'includes/class-dockline-suite-i18n.php';
 
 		/**
 		 * The class responsible for defining all actions that occur in the admin area.
 		 */
-		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'admin/class-dockline-suite-admin.php';
+		require_once plugin_dir_path(dirname(__FILE__)) . 'admin/class-dockline-suite-admin.php';
 
 		/**
 		 * The class responsible for defining all actions that occur in the public-facing
 		 * side of the site.
 		 */
-		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'public/class-dockline-suite-public.php';
+		require_once plugin_dir_path(dirname(__FILE__)) . 'public/class-dockline-suite-public.php';
 
 		$this->loader = new Dockline_Suite_Loader();
-
 	}
 
 	/**
@@ -135,12 +137,12 @@ class Dockline_Suite {
 	 * @since    1.0.0
 	 * @access   private
 	 */
-	private function set_locale() {
+	private function set_locale()
+	{
 
 		$plugin_i18n = new Dockline_Suite_i18n();
 
-		$this->loader->add_action( 'plugins_loaded', $plugin_i18n, 'load_plugin_textdomain' );
-
+		$this->loader->add_action('plugins_loaded', $plugin_i18n, 'load_plugin_textdomain');
 	}
 
 	/**
@@ -150,13 +152,13 @@ class Dockline_Suite {
 	 * @since    1.0.0
 	 * @access   private
 	 */
-	private function define_admin_hooks() {
+	private function define_admin_hooks()
+	{
 
-		$plugin_admin = new Dockline_Suite_Admin( $this->get_plugin_name(), $this->get_version() );
+		$plugin_admin = new Dockline_Suite_Admin($this->get_plugin_name(), $this->get_version());
 
-		$this->loader->add_action( 'admin_enqueue_scripts', $plugin_admin, 'enqueue_styles' );
-		$this->loader->add_action( 'admin_enqueue_scripts', $plugin_admin, 'enqueue_scripts' );
-
+		$this->loader->add_action('admin_enqueue_scripts', $plugin_admin, 'enqueue_styles');
+		$this->loader->add_action('admin_enqueue_scripts', $plugin_admin, 'enqueue_scripts');
 	}
 
 	/**
@@ -166,13 +168,29 @@ class Dockline_Suite {
 	 * @since    1.0.0
 	 * @access   private
 	 */
-	private function define_public_hooks() {
+	private function define_public_hooks()
+	{
 
-		$plugin_public = new Dockline_Suite_Public( $this->get_plugin_name(), $this->get_version() );
+		$plugin_public = new Dockline_Suite_Public($this->get_plugin_name(), $this->get_version());
 
-		$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_styles' );
-		$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_scripts' );
+		$this->loader->add_action('wp_enqueue_scripts', $plugin_public, 'enqueue_styles');
+		$this->loader->add_action('wp_enqueue_scripts', $plugin_public, 'enqueue_scripts');
+	}
 
+	/**
+	 * Register all of the hooks related to the updater
+	 *
+	 * @since    1.0.0
+	 * @access   private
+	 */
+	private function define_update_hooks()
+	{
+
+		$plugin_updater = new Dockline_Suite_GitHub_Updater($this->get_plugin_name(), $this->get_version());
+
+		$this->loader->add_filter('plugins_api', $plugin_updater, 'info', 20, 3);
+		$this->loader->add_filter('site_transient_update_plugins', $plugin_updater, 'update');
+		$this->loader->add_action('upgrader_process_complete', $plugin_updater, 'purge', 10, 2);
 	}
 
 	/**
@@ -180,7 +198,8 @@ class Dockline_Suite {
 	 *
 	 * @since    1.0.0
 	 */
-	public function run() {
+	public function run()
+	{
 		$this->loader->run();
 	}
 
@@ -191,7 +210,8 @@ class Dockline_Suite {
 	 * @since     1.0.0
 	 * @return    string    The name of the plugin.
 	 */
-	public function get_plugin_name() {
+	public function get_plugin_name()
+	{
 		return $this->plugin_name;
 	}
 
@@ -201,7 +221,8 @@ class Dockline_Suite {
 	 * @since     1.0.0
 	 * @return    Dockline_Suite_Loader    Orchestrates the hooks of the plugin.
 	 */
-	public function get_loader() {
+	public function get_loader()
+	{
 		return $this->loader;
 	}
 
@@ -211,8 +232,8 @@ class Dockline_Suite {
 	 * @since     1.0.0
 	 * @return    string    The version number of the plugin.
 	 */
-	public function get_version() {
+	public function get_version()
+	{
 		return $this->version;
 	}
-
 }
